18a97324f17578c923832ad60cc02bec95087728
Abner Willys <abwillys001@gmail.com>
2021-10-29 00:01:25 -0300

    added fonts and conditional to open the application only after fonts are loaded, and setting the status bar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import AppLoading from 'expo-app-loading'
+import { StatusBar } from 'expo-status-bar'
 
 import { Home } from './src/screens/Home'
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  if (!isFontsLoaded) {
+    return <AppLoading />
+  }
+  
   return (
-    <Home />
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
   );
 }
