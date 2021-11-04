c8bcd68d43148989e8c10a61ba5705807defa92f
Abner Willys <abwillys001@gmail.com>
2021-11-04 01:06:35 -0300

    header of the app

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-
 import {
   Text,
+  TouchableOpacity,
   View
 } from 'react-native';
+
+import { UserPhoto } from '../UserPhoto';
 
 import { styles } from './styles';
 
 import Logo from '../../assets/logo.svg'
 
-export function Header(){
+export function Header() {
   return (
     <View style={styles.container}>
       <Logo />
-      
-      <Text>Sair</Text>
+
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+
+        <UserPhoto imageUri='https://github.com/abner-starkasty.png' />
+      </View>
+
     </View>
   );
 }
